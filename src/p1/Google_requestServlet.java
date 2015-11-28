2f64e75adc07a02ccd385a41945f4911e647f694
XyLnEn <le_pingouin_dbil@hotmail.fr>
2015-11-28 21:47:56 +0100

    ajout de l'enrichissement en dur

--- src/p1/Google_requestServlet.java
+++ src/p1/Google_requestServlet.java
@@ -51,17 +51,161 @@ public class Google_requestServlet extends HttpServlet {
 				// TODO Auto-generated catch block
 				//e.printStackTrace();
 			}
+			System.out.println("lol");
 			
-//			
+//			JSONObject jsonresp = null;
+//			JSONArray arr = null;
 //			
 //		    for (int i = 0; i < json.optJSONObject("responseData").optJSONArray("results").length(); i++) {
 //		        try {
-//		        	arr.put(json.optJSONObject("responseData").optJSONArray("results").get(i));
+//		        	
+//					arr.put(json.optJSONObject("responseData").optJSONArray("results").get(i));
 //				} catch (JSONException e) {
 //					// TODO Auto-generated catch block
 //					e.printStackTrace();
 //				}
 //		    }
+//		    
+//		    try {
+//				jsonresp.accumulate("results", arr);
+//			} catch (JSONException e1) {
+//				// TODO Auto-generated catch block
+//				e1.printStackTrace();
+//			}
+		    
+			//--------------------------------------------------------------
+		    //on envoie les liens vers le servlet enrichisseur
+		    //retour d'un jsonObject "retour" contenant un jsonarray "enrichie" ac les 4 elements (0-3), ac chaque element etant un jsonObject "URLenv" et un JSONArray "amenagement" contenant la liste des ammenagements
+		    
+		    //------------
+		    //pour tester, on suppose que l'enrichissement a été effectué, on a donc ce json en retour:
+		    JSONArray retour = null;
+		    JSONObject o11 = new JSONObject();
+		    JSONObject o22 = new JSONObject();
+		    JSONObject o33 = new JSONObject();
+		    JSONObject o44 = new JSONObject();
+		    JSONArray o1 = new JSONArray();
+		    JSONArray o2 = new JSONArray();
+		    JSONArray o3 = new JSONArray();
+		    JSONArray o4 = new JSONArray();
+		    try {
+		    	//1er elem
+
+				o33.put("URL", "en.wikipedia.org");
+
+			    JSONObject val1 = new JSONObject();
+				val1.put("nom", "assensseur3");
+				val1.put("descr", "ce batiment est equipé d'un assensseur3");
+			    JSONObject val2 = new JSONObject();
+				val2.put("nom", "porte");
+				val2.put("descr", "ce batiment a des portes larges3");
+				o3.put(0, val1);
+				o3.put(1, val2);
+
+				//voila la forme du json recu pour le 1er lien, il faut donc maintenant inserer l'info:
+//				JSONObject three = (JSONObject) json.optJSONObject("responseData").optJSONArray("results").get(2);
+//				o33.put("equipement", o3);
+//				
+//				three.put("resp", o33);
+				//-----retour.put(2, three);
+				//2eme elem
+				
+				o11.put("URL", "www.amazon.com");
+
+			    val1 = new JSONObject();
+				val1.put("nom", "assensseur2");
+				val1.put("descr", "ce batiment est equipé d'un assensseur1");
+			    val2 = new JSONObject();
+				val2.put("nom", "porte");
+				val2.put("descr", "ce batiment a des portes larges1");
+				o1.put(0, val1);
+				o1.put(1, val2);
+				//voila la forme du json recu pour le 1er lien, il faut donc maintenant inserer l'info:
+//				JSONObject one = (JSONObject) json.optJSONObject("responseData").optJSONArray("results").get(0);
+//				o11.put("equipement", o2);
+//				
+//				one.put("resp", o22);
+//				retour.put(0, one);
+				//3eme elem
+				
+				o22.put("URL", "twitter.com");
+
+			    val1 = new JSONObject();
+				val1.put("nom", "assensseur2");
+				val1.put("descr", "ce batiment est equipé d'un assensseur2");
+			    val2 = new JSONObject();
+				val2.put("nom", "porte");
+				val2.put("descr", "ce batiment a des portes larges2");
+				o2.put(0, val1);
+				o2.put(1, val2);
+				//voila la forme du json recu pour le 1er lien, il faut donc maintenant inserer l'info:
+//				JSONObject two = (JSONObject) json.optJSONObject("responseData").optJSONArray("results").get(1);
+//				o22.put("equipement", o2);
+//				
+//				two.put("resp", o22);
+//				retour.put(1, two);
+				//retour.put(2, three);
+				//4eme elem
+				
+				o44.put("URL", "boards.4chan.org");
+
+			    val1 = new JSONObject();
+				val1.put("nom", "assensseur4");
+				val1.put("descr", "ce batiment est equipé d'un assensseur4");
+			    val2 = new JSONObject();
+				val2.put("nom", "porte");
+				val2.put("descr", "ce batiment a des portes larges4");
+				o4.put(0, val1);
+				o4.put(1, val2);
+				//voila la forme du json recu pour le 1er lien, il faut donc maintenant inserer l'info:
+//				JSONObject four = (JSONObject) json.optJSONObject("responseData").optJSONArray("results").get(3);
+//				o44.put("equipement", o4);
+//				
+//				four.put("resp", o44);
+//				retour.put(3, four);
+				
+				
+				
+				JSONObject one = (JSONObject) json.optJSONObject("responseData").optJSONArray("results").get(0);
+				o11.put("equipement", o2);
+				one.put("resp", o22);
+				//retour.put(0, one);
+				
+				JSONObject two = (JSONObject) json.optJSONObject("responseData").optJSONArray("results").get(1);
+				o22.put("equipement", o2);
+				two.put("resp", o22);
+				//retour.put(1, two);
+				
+				JSONObject three = (JSONObject) json.optJSONObject("responseData").optJSONArray("results").get(2);
+				o33.put("equipement", o3);
+				three.put("resp", o33);
+				//retour.put(2, three);
+				
+				JSONObject four = (JSONObject) json.optJSONObject("responseData").optJSONArray("results").get(3);
+				o44.put("equipement", o4);
+				four.put("resp", o44);
+				//retour.put(3, four);
+				
+				json.getJSONObject("responseData").remove("results");
+				json.getJSONObject("responseData").append("results",one);
+				//json.getJSONObject("responseData").getJSONArray("results").put(one);
+				json.getJSONObject("responseData").getJSONArray("results").put(two);
+				json.getJSONObject("responseData").getJSONArray("results").put(three);
+				json.getJSONObject("responseData").getJSONArray("results").put(four);
+				
+				//out.print(json);
+				
+				
+				
+				//----------------------------------------------------------------------------
+			} catch (JSONException e1) {
+				e1.printStackTrace();
+			}
+		    
+		    
+		    
+		    
+		    
 //		    
 //		    url = new URL(
 //				    "https://ajax.googleapis.com/ajax/services/search/web?v=1.0&"
@@ -98,6 +242,7 @@ public class Google_requestServlet extends HttpServlet {
 				//json2.put("nextpage", nbtonextpage+8);
 				json.put("nextpage", nbtonextpage+4);
 				json.put("lastpage", nbtonextpage-4);
+				
 			} catch (JSONException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
